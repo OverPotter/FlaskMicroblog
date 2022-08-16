1caad6b054c2113ba6d43610b1fa40264f215bc6
Qd Miracle <qdmiracle21@gmail.com>
2022-08-16 13:24:44 +0300

    time ISO 8601 with moment.js from Flask Moment

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from flask_moment import Moment
 
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -20,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
